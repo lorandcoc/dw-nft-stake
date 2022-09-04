5c5725079380a6f5a3f098cafd726fe9a29c77d4
lorandcoc <lorand.83@gmail.com>
2022-09-04 23:03:35 +0300

    first commit

---
+++ pages/mint.tsx
@@ -0,0 +1,59 @@
+import { useAddress, useMetamask, useNFTDrop } from "@thirdweb-dev/react";
+import type { NextPage } from "next";
+import { useRouter } from "next/router";
+import styles from "../styles/Home.module.css";
+
+const Mint: NextPage = () => {
+  const router = useRouter();
+  // Get the currently connected wallet's address
+  const address = useAddress();
+
+  // Function to connect to the user's Metamask wallet
+  const connectWithMetamask = useMetamask();
+
+  // Get the NFT Collection contract
+  const nftDropContract = useNFTDrop(
+    "0xfA9F75eD253B46F4FA3728116Cb6C4661aD6dAbb"
+  );
+
+  async function claimNft() {
+    try {
+      const tx = await nftDropContract?.claim(1);
+      console.log(tx);
+      alert("NFT Claimed!");
+      router.push(`/stake`);
+    } catch (error) {
+      console.error(error);
+      alert(error);
+    }
+  }
+
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.h1}>Mint A Devil!</h1>
+
+      <p className={styles.explain}>
+        Click the button below to mint your devil!
+      </p>
+      <hr className={`${styles.smallDivider} ${styles.detailPageHr}`} />
+
+      {!address ? (
+        <button
+          className={`${styles.mainButton} ${styles.spacerBottom}`}
+          onClick={connectWithMetamask}
+        >
+          Connect Wallet
+        </button>
+      ) : (
+        <button
+          className={`${styles.mainButton} ${styles.spacerBottom}`}
+          onClick={() => claimNft()}
+        >
+          Claim A Devil
+        </button>
+      )}
+    </div>
+  );
+};
+
+export default Mint;
