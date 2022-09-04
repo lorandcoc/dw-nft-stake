1941827f29046268c4f777ef4ebea4cfcb2557ae
lorandcoc <lorand.83@gmail.com>
2022-09-04 23:56:16 +0300

    title update

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,7 @@ const Home: NextPage = () => {
   return (
     <div className={styles.container}>
       {/* Top Section */}
-      <h1 className={styles.h1}>thirdweb Deploy - Custom Staking Contract</h1>
+      <h1 className={styles.h1}>DevilWears Staking App</h1>
       <div className={styles.nftBoxGrid}>
         <div
           className={styles.optionSelectBox}
